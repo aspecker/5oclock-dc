2eb98c54730d8d64db14dbb365b68db5adb32be3
Adam Specker <specker@gmail.com>
2018-04-17 12:48:46 -0400

    testing APIs

--- data/apiCalls.js
+++ data/apiCalls.js
@@ -1,5 +1,18 @@
 const axios = require('axios');
 const fs = require('fs');
+const keys =  require('./apiKeys.js');
+// API Keys - import and define
+const {yelpID, yelpAPIKey, fourSqID, fourSqSecret, gMapsAPIKey} = keys;
 
-const yelpID = '1iCb98ORr0Vu7LXwTv0xrA';
-const yelpAPIKey = 'LwF5XKAJD5eVmevCodNLwK_MLiQ0NxEV8zeArXd6W8ttpU575B_wnA-nALntsKvu3tEnZ50nr-QC1_Q6_SJAPVyqTfxXG3wxTASUn5wkpeU40im8PHVr7LmMzx3WWnYx'
+console.log(yelpID, yelpAPIKey, fourSqID, fourSqSecret, gMapsAPIKey)
+
+// NOTES
+// YELP 
+// - is basic but easy to use
+// - could fetch simple information about bars and populate a list of bars
+
+// FOURSQUARE
+// 
+
+
+// TEST CALLS
