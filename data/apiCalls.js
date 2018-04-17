402c16ebae2b615cab45a295662138c92ff8bf2f
Adam Specker <specker@gmail.com>
2018-04-17 12:22:30 -0400

    testing various APIs to gather venue info

---
+++ data/apiCalls.js
@@ -0,0 +1,5 @@
+const axios = require('axios');
+const fs = require('fs');
+
+const yelpID = '1iCb98ORr0Vu7LXwTv0xrA';
+const yelpAPIKey = 'LwF5XKAJD5eVmevCodNLwK_MLiQ0NxEV8zeArXd6W8ttpU575B_wnA-nALntsKvu3tEnZ50nr-QC1_Q6_SJAPVyqTfxXG3wxTASUn5wkpeU40im8PHVr7LmMzx3WWnYx'
