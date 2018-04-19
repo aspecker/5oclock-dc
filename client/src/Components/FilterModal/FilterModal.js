6ae43280dbc3eab5311d9ab64923424fce7ca28b
Adrian Kyzer <adriankyzer@gmail.com>
2018-04-19 10:27:45 -0400

    started styling the filter modal

--- client/src/Components/FilterModal/FilterModal.js
+++ client/src/Components/FilterModal/FilterModal.js
@@ -62,92 +62,94 @@ class FilterModal extends Component {
 
   render() {
     return (
-      <div className='filter-modal-container'>
-        <div className='modal-content'>
-          <h3>Narrow Your Search</h3>
-          <p>* All fields are optional</p>
-          <div className='price-radio'>
-              <h4 className='filter-item'>Price:</h4>
-              <form onSubmit={this.handleFormSubmit}>
-                <div className="radio">
-                  <label>
-                    <input type="radio" value="$"
-                    checked={this.state.selectedPriceOption === '$'}
-                    onChange={this.handlePriceOptionChange} />
-                    $
-                  </label>
-                </div>
-                <div className="radio">
-                  <label>
-                    <input type="radio" value="$$"
-                    checked={this.state.selectedPriceOption === '$$'}
-                    onChange={this.handlePriceOptionChange} />
-                    $$
-                  </label>
-                </div>
-                <div className="radio">
-                  <label>
-                    <input type="radio" value="$$$"
-                    checked={this.state.selectedPriceOption === '$$$'}
-                    onChange={this.handlePriceOptionChange} />
-                    $$$
-                  </label>
-                </div>
-              </form>
-            </div>
-            <Dropdown>{this.state.cuisineArr} Choose your cuisine type...</Dropdown>
-            <div className='booze-radio'>
-                <h4 className='filter-item'>Drink Type:</h4>
+      <div className="filter-modal">
+        <div className='filter-modal-container'>
+          <div className='modal-content'>
+            <h3>Narrow Your Search</h3>
+            <p>* All fields are optional</p>
+            <div className='price-radio'>
+                <h4 className='filter-item'>Price:</h4>
                 <form onSubmit={this.handleFormSubmit}>
                   <div className="radio">
                     <label>
-                      <input type="radio" value="Beer"
-                      checked={this.state.selectedDrinkOption === 'Beer'}
-                      onChange={this.handleDrinkOptionChange} />
-                      Beer
+                      <input type="radio" value="$"
+                      checked={this.state.selectedPriceOption === '$'}
+                      onChange={this.handlePriceOptionChange} />
+                      $
                     </label>
                   </div>
                   <div className="radio">
                     <label>
-                      <input type="radio" value="Wine"
-                      checked={this.state.selectedDrinkOption === 'Wine'}
-                      onChange={this.handleDrinkOptionChange} />
-                      Wine
+                      <input type="radio" value="$$"
+                      checked={this.state.selectedPriceOption === '$$'}
+                      onChange={this.handlePriceOptionChange} />
+                      $$
                     </label>
                   </div>
                   <div className="radio">
                     <label>
-                      <input type="radio" value="Cocktail"
-                      checked={this.state.selectedDrinkOption === 'Cocktail'}
-                      onChange={this.handleDrinkOptionChange} />
-                      Cocktail
+                      <input type="radio" value="$$$"
+                      checked={this.state.selectedPriceOption === '$$$'}
+                      onChange={this.handlePriceOptionChange} />
+                      $$$
                     </label>
                   </div>
                 </form>
               </div>
-              <div className='time-radio'>
-                <h4 className='filter-item'>Hours:</h4>
-                <form onSubmit={this.handleFormSubmit}>
-                  <div className="radio">
-                    <label>
-                      <input type="radio" value="Before4"
-                      checked={this.state.selectedTimeOption === 'Before4'}
-                      onChange={this.handleTimeOptionChange} />
-                      Before 4PM
-                    </label>
-                  </div>
-                  <div className="radio">
-                    <label>
-                      <input type="radio" value="After7"
-                      checked={this.state.selectedTimeOption === 'After7'}
-                      onChange={this.handleTimeOptionChange} />
-                      After 7PM
-                    </label>
-                  </div>
-                </form>
-              </div>
-          <Button onClick={this.handleFilterSubmit}>Submit</Button>
-          <Button onClick={this.handleFormReset}>Reset</Button>
+              <Dropdown>{this.state.cuisineArr} Choose your cuisine type...</Dropdown>
+              <div className='booze-radio'>
+                  <h4 className='filter-item'>Drink Type:</h4>
+                  <form onSubmit={this.handleFormSubmit}>
+                    <div className="radio">
+                      <label>
+                        <input type="radio" value="Beer"
+                        checked={this.state.selectedDrinkOption === 'Beer'}
+                        onChange={this.handleDrinkOptionChange} />
+                        Beer
+                      </label>
+                    </div>
+                    <div className="radio">
+                      <label>
+                        <input type="radio" value="Wine"
+                        checked={this.state.selectedDrinkOption === 'Wine'}
+                        onChange={this.handleDrinkOptionChange} />
+                        Wine
+                      </label>
+                    </div>
+                    <div className="radio">
+                      <label>
+                        <input type="radio" value="Cocktail"
+                        checked={this.state.selectedDrinkOption === 'Cocktail'}
+                        onChange={this.handleDrinkOptionChange} />
+                        Cocktail
+                      </label>
+                    </div>
+                  </form>
+                </div>
+                <div className='time-radio'>
+                  <h4 className='filter-item'>Hours:</h4>
+                  <form onSubmit={this.handleFormSubmit}>
+                    <div className="radio">
+                      <label>
+                        <input type="radio" value="Before4"
+                        checked={this.state.selectedTimeOption === 'Before4'}
+                        onChange={this.handleTimeOptionChange} />
+                        Before 4PM
+                      </label>
+                    </div>
+                    <div className="radio">
+                      <label>
+                        <input type="radio" value="After7"
+                        checked={this.state.selectedTimeOption === 'After7'}
+                        onChange={this.handleTimeOptionChange} />
+                        After 7PM
+                      </label>
+                    </div>
+                  </form>
+                </div>
+            <Button onClick={this.handleFilterSubmit}>Submit</Button>
+            <Button onClick={this.handleFormReset}>Reset</Button>
+          </div>
         </div>
       </div>
     )
