edd414b8ed1ed760b561c4d8c6a414ac85debab5
Yoon <Yoon@Yoons-MacBook-Pro-2.local>
2018-04-23 10:45:08 -0400

    footer slide about

--- client/src/Components/Footer/Footer.js
+++ client/src/Components/Footer/Footer.js
@@ -3,7 +3,18 @@ import "./Footer.css";
 
 const Footer = () => (
   <footer className="footer">
-
+  <div className="">
+    <h3>Contact Us</h3>
+  </div>
+    <div>
+      <i class="fab fa-github"><a src="https://github.com/aspecker/5oclock-dc"></a></i>
+    </div>
+    <div>Instagram</div>
+    <div>
+      <i class="fas fa-envelope">
+      </i>
+    </div>
+   
   </footer>
 );
 
