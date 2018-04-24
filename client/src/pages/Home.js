e037ebb344ab960a206f8a3db32d5cf0f559a793
Adrian Kyzer <adriankyzer@gmail.com>
2018-04-24 00:52:23 -0400

    added logo

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -6,6 +6,8 @@ import NeighborhoodDropDown from "../Components/Search/NeighborhoodDropDown";
 import Footer from "../Components/Footer";
 import Genesis from './gensis.jpeg';
 import Background from './greybg.jpg';
+// import Logo from "../Components/Logo";
+import LogoImg from './five-pm-logo.png';
 
 
 const wrapperStyle = {
@@ -13,10 +15,19 @@ const wrapperStyle = {
   backgroundRepeat: 'repeat',
   minHeight: '100vh'
 };
+
+const logoStyle = {
+  width: '100px',
+  position: 'fixed',
+  top: '10px',
+  left: '10px',
+  zIndex: 10
+}
 // attempt to fix deploy problems
 const Home = () =>(
 
     <Wrapper style={wrapperStyle}>
+      <img className='img img-responsive img-fluid' style={logoStyle} src={LogoImg} alt='logo' />
       <Hero backgroundImage={Genesis}>
         <h1>Find Your Happy Hour</h1>
         <NeighborhoodDropDown />
