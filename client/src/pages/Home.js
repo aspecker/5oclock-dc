0b568182c0d095d92b591ffc5c3e39057d23c5b9
Yoon <Yoon@Yoons-MacBook-Pro-2.local>
2018-04-23 23:38:05 -0400

    carousel and new styling

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,18 +2,23 @@ import React from "react";
 import Wrapper from "../Components/Wrapper";
 import Hero from "../Components/Hero";
 // import About from '../components/About';
+import NicoleSos from "../Components/Carousel";
 import NeighborhoodDropDown from "../Components/Search/NeighborhoodDropDown";
 import Footer from "../Components/Footer";
 import Genesis from './gensis.jpeg';
+import "./Home.css";
 
 // attempt to fix deploy problems
 
 const Home = () =>(
       <Wrapper>
-        <Hero backgroundImage={Genesis}>
+        {/* <Hero backgroundImage={Genesis}> */}
+        <NicoleSos />
+        <div className="dropDrop">
           <h1>Find Your Happy Hour</h1>
           <NeighborhoodDropDown />
-        </Hero>
+        </div>
+        {/* </Hero> */}
         {/* <About /> */}
         <Footer />
       </Wrapper>
