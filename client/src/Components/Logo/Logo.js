501c49978372ef886c458447b746067f2f189441
Marucs Hilaire <marcushilaire@The-Mac.local>
2018-04-24 12:32:26 -0400

    link to home

--- client/src/Components/Logo/Logo.js
+++ client/src/Components/Logo/Logo.js
@@ -1,11 +1,19 @@
 import React from "react";
 import './Logo.css';
 import FivePM from './five-pm-logo.png';
+import { Link } from 'react-router-dom'
 
-const Logo = props => (
-  <div className='logo'>
-   <img className='img img-responsive img-fluid logo-img' src={ FivePM } alt='logo' />
-  </div>
 
-);
+class Logo extends Component{
+  render(){
+  return(
+  
+    <div className='logo'>
+      <Link to="/">
+        <img className='img img-responsive img-fluid logo-img' src={ FivePM } alt='logo' />
+      </Link>
+    </div>
+
+)};
+}
 export default Logo;
