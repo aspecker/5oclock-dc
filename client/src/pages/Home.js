523e69de72f8c18f9d9eff56f329046a3705232f
Marcus Hilaire <marcushilaire@gmail.com>
2018-04-24 12:04:29 -0400

    Update Home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,20 +1,13 @@
 import React from "react";
 import Wrapper from "../Components/Wrapper";
-import Hero from "../Components/Hero";
-<<<<<<< HEAD
-import About from '../Components/About';
 import Slide from "../Components/Carousel";
-=======
-import NicoleSos from "../Components/Carousel";
 import About from './About';
->>>>>>> 1567eff40ed84e4f2c51c1af1bf69abb37c1b5ad
 import NeighborhoodDropDown from "../Components/Search/NeighborhoodDropDown";
 import Footer from "../Components/Footer";
-import Genesis from './gensis.jpeg';
 import "./Home.css";
 import Background from './greybg.jpg';
 import Logo from "../Components/Logo";
-// import LogoImg from './five-pm-logo.png';
+
 
 
 const wrapperStyle = {
@@ -25,19 +18,11 @@ const wrapperStyle = {
 
 const Home = () =>(
       <Wrapper>
-<<<<<<< HEAD
         <div className="dropDown">
           <h1>Find Your Happy Hour</h1>
           <NeighborhoodDropDown />
         </div>
         <Slide />
-=======
-        <NicoleSos />
-        <div className="dropDrop">
-          <h1>Find Your Happy Hour</h1>
-          <NeighborhoodDropDown />
-        </div>
->>>>>>> 1567eff40ed84e4f2c51c1af1bf69abb37c1b5ad
         <About />
         <Footer />
       </Wrapper>
