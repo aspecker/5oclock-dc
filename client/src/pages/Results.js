67ec739532b41cadd32ed0a00312daf806b69450
Marcus Hilaire <marcushilaire@gmail.com>
2018-05-14 13:39:03 -0400

    test button working to rewrite array that the page renders from

--- client/src/pages/Results.js
+++ client/src/pages/Results.js
@@ -5,97 +5,112 @@ import Container from "../Components/Container";
 import Row from "../Components/Row";
 import Col from "../Components/Col";
 import BarCard from "../Components/BarCard";
-import Footer from '../Components/Footer';
-import { Button } from '../Components/Search/Button';
-import API from '../utils/API'
-import { Link } from 'react-router-dom'
+import Footer from "../Components/Footer";
+import { Button } from "../Components/Search/Button";
+import API from "../utils/API";
+import { Link } from "react-router-dom";
 import "./Results.css";
-import neighborhoods from '../data/neighborhoods.js'
+import neighborhoods from "../data/neighborhoods.js";
 import Background from "./greybg.jpg";
 import Logo from "../Components/Logo";
 
-
-
 const wrapperStyle = {
   backgroundImage: `url(${Background})`,
-  backgroundRepeat: 'repeat'
+  backgroundRepeat: "repeat"
 };
 
 const heroStyle = {
-  fontSize: '1rem'
+  fontSize: "1rem"
 };
-
 
 class Results extends Component {
   state = {
     bars: []
-  }
+  };
 
-  componentDidMount(){
-    window.scrollTo(0,150);
+  componentDidMount() {
+    window.scrollTo(0, 150);
     this.fetchBars(this.props.match.params.neighborhood);
+    this.mounted = true;
+  }
+  componentWillUnmount() {
+    this.mounted == false;
   }
 
-  fetchBars(neighborhood){
-     API.queryNeighborhood(neighborhood)
-     .then(res =>{
-      this.setState({bars: res.data})
+  fetchBars(neighborhood) {
+    API.queryNeighborhood(neighborhood)
+      .then(res => {
+        this.setState({ bars: res.data });
       })
-      .catch(err=> console.log(err));
-
+      .catch(err => console.log(err));
+  }
+  filterBars(neighborhood, cuisine, price) {
+    API.queryFilters(neighborhood, cuisine, price).then(res => {
+      console.log(res.data);
+      this.setState({ bars: res.data });
+      console.log(this.state.bars);
+    });
+  }
+  findNeighborhoodImage(neighborhood) {
+    return neighborhoods.filter(hood => hood.name === neighborhood);
   }
 
-  findNeighborhoodImage(neighborhood){
-    return neighborhoods.filter(hood=>hood.name===neighborhood)
-  }
-
-  convertPrice(int){
-    switch (int){
+  convertPrice(int) {
+    switch (int) {
       case 1:
-        return '$'
+        return "$";
       case 2:
-        return  '$$'
+        return "$$";
       case 3:
-        return '$$$'
+        return "$$$";
       default:
-        return 'No Price Available'
+        return "No Price Available";
     }
   }
 
-
   render() {
     return (
-
       <Wrapper style={wrapperStyle}>
-      <Logo />
-        <Hero style={heroStyle} backgroundImage= {this.findNeighborhoodImage(this.props.match.params.neighborhood)[0].image}>
+        <Logo />
+        <Hero
+          style={heroStyle}
+          backgroundImage={
+            this.findNeighborhoodImage(this.props.match.params.neighborhood)[0]
+              .image
+          }
+        >
           <h1>{this.props.match.params.neighborhood}</h1>
-          <Link to = '/'>
+          <Link to="/">
             <Button> Change Neighborhood</Button>
           </Link>
         </Hero>
         <div className="card-results-wrapper">
           <Container>
-            <Row >
-            {this.state.bars.map(bar=>
-              <Col size='sm-6 md-4'>
-                <div className='card-columns'>
-                    <Link key={bar._id}
-                    to ={`/bar/${bar._id}`}>
-                    <BarCard key={bar._id} id={bar._id}
-                      backgroundImage={bar.image}
-                      image={bar.image}
-                      name={bar.name}
-                      cuisine={bar.cuisine}
-                      neighborhood={bar.neighborhood}
-                      startTime={bar.startTime}
-                      endTime={bar.endTime}
-                      price={this.convertPrice(bar.price)}>
-                    </BarCard>
+            <button onClick={() => this.filterBars("Georgetown", "null", 2)}>
+              {" "}
+              search for Georgetown bars at 2 pricepoint{" "}
+            </button>
+            <Row>
+              {this.state.bars.map(bar => (
+                <Col size="sm-6 md-4">
+                  <div className="card-columns">
+                    <Link key={bar._id} to={`/bar/${bar._id}`}>
+                      <BarCard
+                        key={bar._id}
+                        id={bar._id}
+                        backgroundImage={bar.image}
+                        image={bar.image}
+                        name={bar.name}
+                        cuisine={bar.cuisine}
+                        neighborhood={bar.neighborhood}
+                        startTime={bar.startTime}
+                        endTime={bar.endTime}
+                        price={this.convertPrice(bar.price)}
+                      />
                     </Link>
-                </div>
-              </Col>
-            )}
+                  </div>
+                </Col>
+              ))}
             </Row>
           </Container>
         </div>
@@ -104,6 +119,5 @@ class Results extends Component {
     ); // end of return
   } // end of render
 } // end of class Results extends Component
-
 
 export default Results;
