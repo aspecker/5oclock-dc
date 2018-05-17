c7fae79369290eb70442da6f1e227ab2e88bc3c7
Adam Specker <specker@gmail.com>
2018-05-16 22:25:42 -0400

    update about

--- client/src/Components/About/About.js
+++ client/src/Components/About/About.js
@@ -13,6 +13,7 @@ const About = props => (
         </h2>
         <p>
           5 O'Clock DC started from the simple desire to enjoy drinks with good people. The creators of 5 O'Clock built something they wanted to use, and wanted to share it with other DC locals. We built 5 O'Clock DC with the consideration that happy hour is often an elusive and missed opportunity, but we’ve changed that. We take advantage of DC's growing restaurant scene and find you the best happy hours. Find your happy place with us.
+          Love our site and want us to know? Email us at <a href='mailto:fiveoclockdc@gmail.com'>'fiveoclockdc@gmail.com</a>
         </p>
     </div>
     </Col>
--- client/src/pages/About/About.js
+++ client/src/pages/About/About.js
@@ -12,8 +12,9 @@ const About = props => (
         About Us
         </h2>
         <p>
-          <strong>5 O' Clock DC</strong> started from the simple desire to enjoy drinks with good people. The creators of 5 O' Clock built something they wanted to use, and wanted to share it with other DC locals. We built 5 O'Clock DC with the consideration that happy hour is often an elusive and missed opportunity, but we’ve changed that. We take advantage of DC's growing restaurant scene and find you the best happy hours. Find your happy place with us.
+          <strong>5 O' Clock DC</strong> started from the simple desire to enjoy good drinks with great people. The creators of 5 O' Clock built something they wanted to use, and wanted to share it with other DC locals. We built 5 O'Clock DC with the consideration that happy hour is often an elusive and missed opportunity, but we’ve changed that. We take advantage of DC's growing restaurant scene and find you the best happy hours. Find your happy place with us.  
         </p>
+        <p> Love our site and want us to know? Email us at <a id='emailUs' href='mailto:fiveoclockdc@gmail.com'>'fiveoclockdc@gmail.com</a></p>
         </div>
     </Col>
   </Row>
