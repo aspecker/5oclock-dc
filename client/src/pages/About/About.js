ed501fb098da73eae40b68370e17a0ac7377a170
Adrian Kyzer <adriankyzer@gmail.com>
2018-05-19 10:39:17 -0400

    fixed footer so it stays at bottom when there's not enough content

--- client/src/pages/About/About.js
+++ client/src/pages/About/About.js
@@ -12,9 +12,9 @@ const About = props => (
         About Us
         </h2>
         <p>
-          <strong>5 O' Clock DC</strong> started from the simple desire to enjoy good drinks with great people. As DC's restaurant scene grows rapidly, it's harder than ever to find the right happy hour. When you're spoiled for choice, we make choosing the right one fast and easy. Find your happy place with <strong>5 O' Clock DC</strong>. 
+          <strong>5 O' Clock DC</strong> started from the simple desire to enjoy good drinks with great people. As DC's restaurant scene grows rapidly, it's harder than ever to find the right happy hour. When you're spoiled for choice, we make choosing the right one fast and easy. Find your happy place with <strong>5 O' Clock DC</strong>.
         </p>
-        <p> Love our site? Have feedback? Email us at <a id='emailUs' href='mailto:5oclockdc@gmail.com'>'5oclockdc@gmail.com</a></p>
+        <p> Love our site? Have feedback? Email us at <a id='emailUs' href='mailto:5oclockdc@gmail.com'>5oclockdc@gmail.com</a></p>
         </div>
     </Col>
   </Row>
